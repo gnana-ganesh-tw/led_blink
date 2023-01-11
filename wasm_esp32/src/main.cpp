3eddc4971f577d7b6f8c42d6b6580fbc4414ce89
Gnana Ganesh <gnana.ganesh@thoughtworks.com>
2023-01-11 15:45:01 +0530

    Add. wasmico esp32 example

---
+++ wasm_esp32/src/main.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+#include "setup.hpp"
+#include "server.hpp"
+
+void setup()
+{
+    // put your setup code here, to run once:
+    startSerial();
+    // disableWDT();
+    printGreetingMessage();
+    startFilesystem();
+    startWifi();
+    initWasmEnvironment();
+    startWebServer();
+    setupTasks("/");
+}
+
+void loop()
+{
+    // put your main code here, to run repeatedly:
+
+    handleServerRequests();
+}
